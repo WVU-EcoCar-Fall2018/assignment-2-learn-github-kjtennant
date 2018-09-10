5fdc476318e8f878b94ba00917f983e3883f56a9
kjtennant <43051731+kjtennant@users.noreply.github.com>
2018-09-10 13:16:53 -0400

    Update hellofunc.c

--- hellofunc.c
+++ hellofunc.c
@@ -1,10 +1,7 @@
-//Copyright 2018 Kurt Tennant
+// "Copyright [2018] <Kurt Tennant>"
 #include <stdio.h>
 #include <hellomake.h>
-
-
 void myPrintHelloMake(void) {
-  printf("Hello makefiles!\n");
-  
-  return;
- }
+printf("Hello makefiles!\n");
+return;
+}
